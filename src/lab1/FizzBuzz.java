0d443e0f448db6f315bf25799210418d0b3725c5
Lucetta <lucetta@Lucettadebijibendiannao.local>
2025-09-08 13:18:24 +0530

    refactored to extract helper method

--- src/lab1/FizzBuzz.java
+++ src/lab1/FizzBuzz.java
@@ -7,7 +7,7 @@ class FizzBuzz {
 
     public static void main(String[] args) {
 
-        for (int i = 1; i < 100; i++) {
+         /*for (int i = 1; i < 100; i++) {
 
             // Find out which numbers divide i.
             boolean divisibleBy3 = i % 3 == 0;
@@ -31,6 +31,27 @@ class FizzBuzz {
                 System.out.println(i);
 
             }
+        }*/
+
+        int j = 1;
+        while (j <= 100) {
+            doFizzBuff(j);
+            j++;
+         }
+
+        }
+
+    private static void doFizzBuff(int j) {
+        boolean divisibleBy3 = j % 3 == 0;
+        boolean divisibleBy5 = j % 5 == 0;
+        if (divisibleBy3 && divisibleBy5) {
+            System.out.println("Fizz Buzz");
+        }  else if (divisibleBy3) {
+            System.out.println("Fizz");
+        } else if (divisibleBy5) {
+            System.out.println("Buzz");
+        }  else {
+            System.out.println(j);
         }
     }
 }
